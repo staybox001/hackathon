d653619f2dbbbfc182eb66c00f6c19429d050c2d
Gabriel Vargas Padilha <gabrielvargaspadilha@gmail.com>
2019-11-30 14:25:24 -0300

    finish

--- storage/framework/views/3091fbdc1ac309f184cd5619e593260935d29f6a.php
+++ storage/framework/views/3091fbdc1ac309f184cd5619e593260935d29f6a.php
@@ -36,15 +36,16 @@
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contato</a>
-      </li>
+      <div class="row">
+          <div class="col-sm-10">
+        <li class="nav-item">
+          <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+        </li>
+          </div>
+          <div class="col-sm-1">
+          <a href="<?php echo e(route('logout')); ?>" class='nav-link'>Logout</a>
+          </div>
+      
     </ul>
 
     <!-- SEARCH FORM -->
