40802972aeb11065426646c749811837c804b42e
JЋøu αмαrαł <JonathanAmarall>
2019-11-30 05:43:32 -0300

    Cadastrando geolocation do motorista

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Entrega;
+use App\Geolocation;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -22,17 +23,23 @@ class ApiController extends Controller
         }
     }
     // recebe id do motorista
-    public function listaEntregas($id_motorista){
+    public function listaEntregas($id_motorista)
+    {
         $entregas = DB::table('entregas')
-        ->where('id_motorista', '=', "$id_motorista")
-        ->join('clientes', 'entregas.id_cliente', '=', 'clientes.id')
-        ->join('produtos', 'entregas.id_produto', '=', 'produtos.id')
-        ->select('entregas.*', 'clientes.nome', 'clientes.endereco', 'clientes.cidade', 'clientes.estado', 'produtos.nome AS prodNome')
-        ->get();
+            ->where('id_motorista', '=', "$id_motorista")
+            ->join('clientes', 'entregas.id_cliente', '=', 'clientes.id')
+            ->join('produtos', 'entregas.id_produto', '=', 'produtos.id')
+            ->select('entregas.*', 'clientes.nome', 'clientes.endereco', 'clientes.cidade', 'clientes.estado', 'produtos.nome AS prodNome')
+            ->get();
         return json_encode($entregas);
     }
 
-    public function postGeolocation(Request $request){
-
+    public function postGeolocation(Request $request)
+    {
+        return Geolocation::create([
+            "latitude" => $request->latitude,
+            "longitude" => $request->longitude,
+            "id_motorista" => $request->id_motorista
+        ]);
     }
 }
