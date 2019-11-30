dc53d0d3775d2c615f555cefd42e5380040c95bc
Lenon Corrêa <lenonac_@hotmail.com>
2019-11-30 04:10:30 -0300

    Entraga: alteração cadastro

--- app/Http/Controllers/EntregaController.php
+++ app/Http/Controllers/EntregaController.php
@@ -50,7 +50,8 @@ class EntregaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Entrega::insert(['id_produto' => $request->produto, 'id_cliente' => $request->cliente, 'id_motorista' => $request->motorista]);
+        return redirect()->route('/andamento');
     }
 
     /**
