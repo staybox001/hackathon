f56492969b674b7610e2dca497c14f7591b59d3e
Gabriel Vargas Padilha <gabrielvargaspadilha@gmail.com>
2019-11-30 04:24:02 -0300

    fix conflito

--- storage/framework/views/3e393cec3ff144ba647151f11184336ee644b7ab.php
+++ storage/framework/views/3e393cec3ff144ba647151f11184336ee644b7ab.php
@@ -1,6 +1,45 @@
 <?php $__env->startSection('content'); ?>
 
-    <h2>andamento</h2>   
+      <div class="container">
+         <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Entregas em situação de andamento</h3>
+              </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+              <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>Data</th>
+                  <th>Produto</th>
+                  <th>Cliente</th>
+                  <th>Motorista</th>
+                  <th>Detalhes</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td>Trident</td>
+                  <td>Internet
+                    Explorer 4.0
+                  </td>
+                  <td>Win 95+</td>
+                  <td> 4</td>
+                  <td>X</td>
+                </tr>
+                <tr>
+                  <td>Trident</td>
+                  <td>Internet
+                    Explorer 5.0
+                  </td>
+                  <td>Win 95+</td>
+                  <td>5</td>
+                  <td>C</td>
+                </tr>
+              </table>
+            </div>
+         </div>            
+      </div> 
 
 <?php $__env->stopSection(); ?>
 <?php echo $__env->make('template.template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/gabriel/hackathon/resources/views/entrega/andamento.blade.php ENDPATH**/ ?>
